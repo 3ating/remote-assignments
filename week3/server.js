73332fc631ef801385d8e7066789388f56c5b3eb
Yiting Lin <etlin1401@gmail.com>
2023-01-19 17:18:00 +0800

    prettier

--- week3/server.js
+++ week3/server.js
@@ -1,10 +1,12 @@
 const express = require("express");
-const app = express();
 const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.set("view engine", "pug");
