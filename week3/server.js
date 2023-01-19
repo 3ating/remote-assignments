ed846b9227ef1c90668b86d57473a97142292811
Yiting Lin <etlin1401@gmail.com>
2023-01-19 16:13:18 +0800

    Add Assignment 5

--- week3/server.js
+++ week3/server.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
+const cookieParser = require("cookie-parser");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
+app.use(cookieParser());
+
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
@@ -25,6 +30,28 @@ app.get("/getData", (req, res) => {
     } else {
         res.send("<h2 style='color: red'>Wrong Parameter</h2>");
     }
+});
+
+// assignment 5
+app.get("/myName", (req, res) => {
+    let userName = req.cookies.userName;
+    if (userName) {
+        res.send(`<h1>Welcome, ${userName}!</h1>`);
+    } else {
+        res.render("form");
+    }
+});
+
+app.get("/trackName", (req, res) => {
+    let queryName = req.query.name;
+    res.cookie("userName", queryName);
+    res.redirect("/myName");
+});
+
+app.post("/trackName", (req, res) => {
+    let userName = req.body.userName;
+    res.cookie("userName", userName);
+    res.redirect("/myName");
 });
 
 const port = 3000;
